f617c6d4a4bf0a5c227ca8c393f6102235502320
Ahmad Moussa <gorillasuncontact@gmail.com>
2024-07-17 10:10:45 +0200

    Add files via upload

---
+++ courseContent.js
@@ -0,0 +1,53 @@
+let courseContent = {
+  week_1: {
+    description: "Intro to Creative Coding with P5JS. Drawing Shapes & Patterns.",
+    content: {
+      overview: '',
+      p5js_history: '',
+      intro_to_p5js: '',
+      flow_of_a_p5_program: '',
+      creating_the_canvas: ``,
+      p5_drawing_functions: ``,
+      the_drawing_stack: ``,
+      creating_a_grid: ``,
+      making_patterns_with_grids: ``,
+      circle_packing: ``,
+    },
+    content2: {
+
+    }
+  },
+  week_2: {
+    description: "Interactivity & Animation. A First Look at Generative Art.",
+    content: {
+      overview: ``,
+      interactivity: ``,
+      animation: ``,
+    },
+    content2: {
+
+    }
+  },
+  project: {
+    description: "Overview & Info about the Course Project",
+    content: {
+      overview: ``,
+      genart2: ``,
+      animation: ``,
+    },
+    content2: {
+
+    }
+  },
+  gallery: {
+    description: "Showcase of the Submitted Course Projects",
+    content: {
+      overview: ``,
+      genart: ``,
+      animation: ``,
+    },
+    content2: {
+
+    }
+  },
+}
